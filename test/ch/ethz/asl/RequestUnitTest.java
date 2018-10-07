97b86d991ed296818f13391a9613fa6c24cb7fd8
Florian Morath <morathflorian@gmail.com>
2018-10-07 11:09:22 +0200

    RequestUnitTest: parseRequest

--- test/ch/ethz/asl/RequestUnitTest.java
+++ test/ch/ethz/asl/RequestUnitTest.java
@@ -34,4 +34,35 @@ class RequestUnitTest {
         assertFalse(Request.endOfLineExists(b5));
 
     }
+
+    @Test
+    void parseRequest() {
+
+        ByteBuffer b1 = ByteBuffer.allocate(100);
+        b1.put("get key1 key2\r\n".getBytes());
+
+        ByteBuffer b2 = ByteBuffer.allocate(100);
+        b2.put("set key1 0 500 30\r\nxxxxxxxxxxxxxxxxxxxx\r\n".getBytes());
+
+        ByteBuffer b3 = ByteBuffer.allocate(100);
+        b3.put("key1 0 500 30\r\nxxxx\r\nxxxxxxxxxxxxxxxx\r\n".getBytes());
+
+        ByteBuffer b4 = ByteBuffer.allocate(100);
+        b4.put("key1 key2".getBytes());
+
+
+        Request req1 = new Request(b1, null);
+        assertTrue(req1.type == Request.Type.GET);
+
+        Request req2 = new Request(b2, null);
+        assertTrue(req2.type == Request.Type.SET);
+
+        Request req3 = new Request(b3, null);
+        assertTrue(req3.type == Request.Type.INVALID);
+
+        Request req4 = new Request(b4, null);
+        assertTrue(req4.type == Request.Type.INVALID);
+
+        
+    }
 }
